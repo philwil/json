0e0c7c28c884bac50af3f2bb33da42d6d6e8b7ad
phil wilshire <sysdcs@gmail.com>
2020-03-03 08:07:18 -0500

    looks promising

---
+++ pswtest/2basic_json__basic_json.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
+int main()
+{
+    // create a JSON array
+  json j1 = {"one", "two", 3, 4.5, false};
+  json j2 = {{"one": "two"}, {"3": 4.5}};
+
+    // create a copy
+    json j2(j1);
+
+    // serialize the JSON array
+    std::cout << j1 << " = " << j2 << '\n';
+    std::cout << std::boolalpha << (j1 == j2) << '\n';
+}
